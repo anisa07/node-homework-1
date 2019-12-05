6013901dda0b1349980fcee587ec6acdd55e5ab9
Anisa Askarova <Anisa.Askarova@massive.co>
2019-12-05 09:05:39 +0300

    use read and write streams

--- task2/index.js
+++ task2/index.js
@@ -1,28 +1,16 @@
-const csvToJson = require('csvtojson');
-const fs = require('fs');
-const util = require('util');
-const forEach = require('async-foreach').forEach;
+import csvToJson from 'csvtojson';
+import fs from 'fs';
+
 const csvFilePath = __dirname + '/csv/example-csv1.csv';
 const txtFilePath = __dirname + '/txt/example-txt1.txt';
-const writeFileAsync = util.promisify(fs.writeFile);
 
-const convertedCsvToJson = async (pathToFile) => {
-    try {
-       return csvToJson().fromFile(pathToFile);
-    } catch (err) {
-        console.log('Error occurs reading and converting csv! ', err)
-    }
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream(txtFilePath);
+
+const errorCatcher = (message, e) => {
+    console.log(`Error ${message} ${e}`);
 };
 
-const convertAndSave = async (pathToFile, savePath) => {
-    try {
-        const json = await convertedCsvToJson(pathToFile);
-        forEach(json, (line) => {
-            writeFileAsync(savePath, JSON.stringify(line)+'\n', { flag: 'a+' });
-        });
-    } catch (err) {
-        console.log('Error occurs writing txt file! ', err);
-    }
-};
-
-convertAndSave(csvFilePath, txtFilePath);
+readStream.on('error', (e) => {errorCatcher('reading file!', e)})
+    .pipe(csvToJson())
+    .pipe(writeStream).on('error', (e) => {errorCatcher('writing file!', e)});
