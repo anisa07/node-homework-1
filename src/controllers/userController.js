470fabd5f451c15377225431657e10d4b85f5b59
Anisa Askarova <Anisa.Askarova@massive.co>
2019-12-16 16:59:29 +0300

    move logic from router to contoller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,29 +1,40 @@
 import { getById, create, update, getAll, softDelete } from '../services/userService';
 
 export class UserController {
-    getUserById(id) {
-        return getById(id);
+    getUserById(request, response) {
+        response.send(getById(request.params.id));
     }
 
-    createUser(userData) {
-        return create(userData);
+    createUser(request, response) {
+        try {
+            response.send(create(request.body));
+        } catch (e) {
+            response.status(400).send(e);
+        }
     }
 
-    updateUser(id, userData) {
-        return update(id, userData);
+    updateUser(request, response) {
+        try {
+            response.send(update(request.params.id, request.body));
+        } catch (e) {
+            console.log(e);
+            response.status(400).send(e);
+        }
     }
 
-    getAutoSuggestUsers(loginSubstring, limit) {
-        if (!!limit) {
-            return getAll()
+    getAutoSuggestUsers(request, response) {
+        const { limit, loginSubstring } = request.query;
+        const autoSuggestedUsers = !!limit
+            ? getAll()
                 .filter(user => user.login.includes(loginSubstring))
                 .sort((userA, userB) => userA.login.localeCompare(userB.login))
-                .slice(0, limit);
-        }
-        return [];
+                .slice(0, limit)
+            : [];
+
+        response.send(autoSuggestedUsers);
     }
 
-    deleteUser(id) {
-        return softDelete(id);
+    deleteUser(request, response) {
+        response.send(softDelete(request.params.id));
     }
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,37 +6,27 @@ const userController = new UserController();
 
 // get user by id
 userRouter.get('/:id', (request, response) => {
-    response.send(userController.getUserById(request.params.id));
+    userController.getUserById(request, response);
 });
 
 // create user
 userRouter.post('/create', (request, response) => {
-    try {
-        response.send(userController.createUser(request.body));
-    } catch (e) {
-        response.status(400).send(e);
-    }
+    userController.createUser(request, response);
 });
 
 // update user
 userRouter.patch('/:id', (request, response) => {
-    try {
-        response.send(userController.updateUser(request.params.id, request.body));
-    } catch (e) {
-        console.log(e);
-        response.status(400).send(e);
-    }
+    userController.updateUser(request, response);
 });
 
 // get auto-suggest user from limit users. sorted by login property and filtered by login substring
 userRouter.get('/', (request, response) => {
-    const { limit, loginSubstring } = request.query;
-    response.send(userController.getAutoSuggestUsers(loginSubstring || '', Number.parseInt(limit, 10) || 0));
+    userController.getAutoSuggestUsers(request, response);
 });
 
 // remove user (soft remove, set isDeleted = true)
 userRouter.delete('/:id', (request, response) => {
-    response.send(userController.deleteUser(request.params.id));
+    userController.deleteUser(request, response);
 });
 
 export { userRouter };
