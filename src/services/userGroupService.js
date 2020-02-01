4fd4597986ceefc3eb82eb509c2560de294f39f5
Anisa Askarova <Anisa.Askarova@massive.co>
2020-02-01 18:38:26 +0300

    TASK 4.3 transaction

--- src/services/userGroupService.js
+++ src/services/userGroupService.js
@@ -1,11 +1,14 @@
 import uuidv4 from 'uuid/v4';
+import Sequelize from "sequelize";
 import {UserGroupModel} from '../models/initModels';
 
 export const getAll = async () => UserGroupModel.findAll() || [];
 
 export const create = async ({ userId, groupId }) => {
-    return UserGroupModel.create({
-        userId,
-        groupId
-    })
+    return UserGroupModel.sequelize.transaction((t) => {
+        return UserGroupModel.create({
+            userId,
+            groupId
+        }, {transaction: t});
+    });
 };
