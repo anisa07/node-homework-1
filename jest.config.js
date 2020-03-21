5e09879fde97534ac23209b082aa56ef34418581
Anisa Askarova <Anisa.Askarova@massive.co>
2020-03-21 21:04:11 +0300

    userController tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "testEnvironment": "node",
+    "coveragePathIgnorePatterns": [
+        "/node_modules/"
+    ],
+    "testMatch": [ "**/__tests__/**/*.js", "**/?(*.)+(spec|test).js" ],
+    "verbose": true
+};
